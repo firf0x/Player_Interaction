30f946afb7408164518199d96929a5274b547d94
Fix <hololsw@gmail.com>
2025-12-30 00:59:57 +0800

    Добавление возможности взаимодействия между объектами

--- Assets/Scripts/Interaction/Interaction.cs
+++ Assets/Scripts/Interaction/Interaction.cs
@@ -1,6 +1,17 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Interaction : MonoBehaviour
 {
+    [SerializeField] private float rayDistance;
+    [SerializeField] private InputAction interactAction;
     
+    private void Update()
+    {
+        if(Physics.Raycast(gameObject.transform.position, gameObject.transform.forward, out var hitInfo, rayDistance ))
+        {
+            if(interactAction.ReadValue<bool>() && hitInfo.collider.gameObject.GetComponent<InteractableComponent>() != null)
+                hitInfo.collider.gameObject.GetComponent<InteractableComponent>();
+        }
+    } 
 }
